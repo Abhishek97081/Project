7826479ec09073fbca77395828ae2f90bcbcfb8e
Abhishek97081 <abhisinghrajput970@gmail.com>
2026-02-02 19:52:10 +0530

    tic tac toe style

---
+++ tic-tac-toe -clone/app.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Tic-Tac-Toe Clone!");
+let music = new Audio("music.mp3");
+let turn=new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+let turn="X";
+const changeTurn=()=>{
+    return turn==="X"?"O":"X";
+}
+// function to check for a win
+const checkWin=()=>{
+
+}
+// Game Logic
+
